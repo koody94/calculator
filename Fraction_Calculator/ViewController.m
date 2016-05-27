85d03b3b11c4900600644839312aace10814d7dc
doyeon koo <koody94@naver.com>
2016-05-27 14:42:29 +0900

    Initial Commit

---
+++ Fraction_Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fraction_Calculator
+//
+//  Created by MF839-013 on 2016. 5. 27..
+//  Copyright © 2016년 SDS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
